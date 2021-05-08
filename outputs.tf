dbceee8cce46aec0febc38a37f82c5cda560977b
Arnold Cartagena <awca22@gmail.com>
2021-05-08 20:33:43 +0200

    a

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,14 @@ output "Jenkins-Master-Node-Public-IP" {
   value = aws_instance.jenkins-master-instance.public_ip
 }
 
-#output "Jenkins-Worker-Node-Public-IP" {
-#  value = {
-#    for instance in aws_instance.jenkins-worker-instance :
-#    instance.id => instance.public_ip
-#  }
-#}
-
-
 output "Jenkins-Worker-Node-Public-IP" {
-  value = aws_instance.jenkins-worker-instance.*.tags => aws_instance.jenkins-worker-instance.*.public_ip
+  value = {
+    for instance in aws_instance.jenkins-worker-instance :
+    instance.id => instance.public_ip
+  }
 }
+
+
+#output "Jenkins-Worker-Node-Public-IP" {
+#  value = aws_instance.jenkins-worker-instance.*.tags => aws_instance.jenkins-worker-instance.*.public_ip
+#}
