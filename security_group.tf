000519c84aec183085d41d6d26047de9858246c9
Arnold Cartagena <awca22@gmail.com>
2021-05-08 18:03:14 +0200

    Added SG and Vars

--- security_group.tf
+++ security_group.tf
@@ -1,5 +1,5 @@
 #Create Security Group for Application Load Balancer, Only TCP/80,TCP/443 and Outbound Access
-resource "aws_security_group" "alb-sg-master-vpc" {
+resource "aws_security_group" "alb-sg-master" {
   provider    = aws.region-master
   name        = "alb-master-sg"
   description = "Allow TCP/443 and TCP/80 traffic"
@@ -29,5 +29,80 @@ resource "aws_security_group" "alb-sg-master-vpc" {
 
   tags = {
     Name = "alb-master"
+  }
+}
+
+#Create Security Group for Master Instance, Only TCP/8080 from ALB,TCP/22 from local ip
+resource "aws_security_group" "instance-sg-master-vpc" {
+  provider    = aws.region-master
+  name        = "instance-master-sg"
+  description = "Allow TCP/8080 and TCP/22"
+  vpc_id      = aws_vpc.vpc_master.id
+
+  ingress {
+    description = "Allow 22 from IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.external_ip]
+  }
+  ingress {
+    description = "Allow 8080 from Application Load Balancer"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb-sg-master.id]
+  }
+  ingress {
+    description = "Allow traffic from us-west-2"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    security_groups = ["192.168.1.0/24"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "instance-master"
+  }
+}
+
+#Create Security Group for Master Instance, Only TCP/8080 from ALB,TCP/22 from local ip
+resource "aws_security_group" "instance-sg-worker-vpc" {
+  provider    = aws.region-worker
+  name        = "instance-worker-sg"
+  description = "Allow TCP/8080 and TCP/22"
+  vpc_id      = aws_vpc.vpc_worker.id
+
+  ingress {
+    description = "Allow 22 from IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.external_ip]
+  }
+  ingress {
+    description = "Allow traffic from us-east-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    security_groups = ["10.0.1.0/24"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "instance-worker"
   }
 }
