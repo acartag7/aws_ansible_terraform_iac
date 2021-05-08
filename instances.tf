658c7bc9d8046a11285895765a7ba3f1ca484d24
Arnold Cartagena <awca22@gmail.com>
2021-05-08 19:34:22 +0200

    typo

--- instances.tf
+++ instances.tf
@@ -1,11 +1,11 @@
 #Get Linux AMI ID using SSM Parameter endpoint in us-east-1
 data "aws_ssm_parameter" "linuxAmi-master" {
     provider = aws.region-master
-    name = "/aws/service/ami-amazon-linux-latest/amazn2-ami-hvm-x86_64-gp2" 
+    name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
 #Get Linux AMI ID using SSM Parameter endpoint in us-west-1
 data "aws_ssm_parameter" "linuxAmi-worker" {
     provider = aws.region-worker
-    name = "/aws/service/ami-amazon-linux-latest/amazn2-ami-hvm-x86_64-gp2" 
+    name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2" 
 }
