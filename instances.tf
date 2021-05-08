47a95170f899f91b77c11b6326e764943cc28fac
Arnold Cartagena <awca22@gmail.com>
2021-05-08 19:43:55 +0200

    added key

--- instances.tf
+++ instances.tf
@@ -9,3 +9,17 @@ data "aws_ssm_parameter" "linuxAmi-worker" {
   provider = aws.region-worker
   name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
+
+#Create key-pair for logging into EC2 in us-east-1
+resource "aws_key_pair" "master-instance-key" {
+  provider   = aws.region-master
+  key_name   = "cloud_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+#Create key-pair for logging into EC2 in us-west-2
+resource "aws_key_pair" "worker-instance-key" {
+  provider   = aws.region-worker
+  key_name   = "cloud_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
