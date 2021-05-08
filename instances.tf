f83f3460f3e270d3c3cdec5710ee53174db469a8
Arnold Cartagena <awca22@gmail.com>
2021-05-08 21:22:44 +0200

    added plays in provisioners

--- instances.tf
+++ instances.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "jenkins-master-instance" {
   provisioner "local-exec" {
     command = <<EOF
 aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id}
-ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
+ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible/master-deploy.yml
 EOF
   }
 }
@@ -68,7 +68,7 @@ resource "aws_instance" "jenkins-worker-instance" {
 provisioner "local-exec" {
     command = <<EOF
 aws --profile ${var.profile} ec2 wait instance-status-ok --region ${var.region-worker} --instance-ids ${self.id}
-ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-worker-sample.yml
+ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible/worker-deploy.yml
 EOF
 }
 }
